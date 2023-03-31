3d3194e06860067be98da86abef285b7b9856325
Jeyavishnu S <sjeyavishnu9865@gmail.com>
2023-03-31 14:13:51 +0530

    more edits

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ A simple REST API uisng Express and MongoDB for strengthening my skills as part 
 
 | Action | HTTP method | End Point | Response |Description |
 |--------|--------|-----------|-------------|------|
-| READ all customers | GET | /api/customers | 200 (OK) | fetch all customers from database. |
-| READ a customer | GET | /api/customers/{id} | 200(OK) | Fetch specific customer from database. |
-| CREATE a customer | POST | /api/customers | 201 (CREATED) | Save a new customer into database. |
+| CREATE a customer | POST | /api/customer | 201 (CREATED) | Save a new customer into database. |
+| READ all customers | GET | /api/customer | 200 (OK) | fetch all customers from database. |
+| READ a customer | GET | /api/customer/{id} | 200(OK) | Fetch specific customer from database. |
 | UPDATE a customer | PUT | /api/customer/{id} | 200 (OK) | Update existing user from database. |
 | DELETE a customer | DELETE | /api/customer/{id} | 204(NO CONTENT) | Delete a specific customer from database.
--- index.js
+++ index.js
@@ -1,10 +1,25 @@
+// dotenv for process environment variables
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+// connect to database simple-rest-api
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to Database!'))
+  .catch((error) => console.log(error));
+
+// setup express middleware
 const app = express();
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.json());
 
-app.get( '/', ( req, res) => {
+// use separate route handler for customers
+const customerRouter = require('./routes/customerRouter');
+app.use('/customer', customerRouter);
+
+// base endpoint for validation
+app.get('/', ( req, res) => {
   res.json({
     'API Name': 'Simple-Rest-API',
     'Author':'S.Jeyavishnu',
@@ -12,4 +27,5 @@ app.get( '/', ( req, res) => {
   })
 });
 
+// bootstrapping server
 app.listen( process.env.PORT || 3000, () => console.log('Server Started at port 3000.'));
